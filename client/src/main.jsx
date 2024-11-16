aa4e36a0fc8ffe3eb3038b42ca6e9d8fbc510db2
Yash Bhut <67946769+YashBhut1524@users.noreply.github.com>
2024-11-16 17:46:38 +0530

    deployment setup completed

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -7,7 +7,7 @@ import GridBackground from './components/ui/GridBackground.jsx'
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 
 const client = new ApolloClient({
-	uri: 'http://localhost:4000/graphql', //url of GraphQL Server
+	uri:import.meta.env.VITE_NODE_ENV === "development"? 'http://localhost:4000/graphql': "/graphql",  //url of GraphQL Server
 	cache: new InMemoryCache(),
 	credentials: "include", //for Apollo Client to send cookies along with every request to the server.
 });
